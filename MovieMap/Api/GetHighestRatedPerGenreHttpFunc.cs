c4e17da84a3bc41990c4489831f799f63979236a
Vsevolod Deribas <vsevolod.deribas@komplettbank.no>
2022-03-27 13:28:55 +0200

    Remove async from method

--- MovieMap/Api/GetHighestRatedPerGenreHttpFunc.cs
+++ MovieMap/Api/GetHighestRatedPerGenreHttpFunc.cs
@@ -4,7 +4,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using MovieMap.Services;
-using System.Threading.Tasks;
 
 namespace MovieMap
 {
@@ -18,18 +17,18 @@ namespace MovieMap
         }
 
         [FunctionName(nameof(GetHighestRatedPerGenreHttpFunc))]
-        public async Task<IActionResult> Run(
+        public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "genres/{genre:alpha:length(1,40)}")] HttpRequest req,
             string genre,
             ILogger log)
         {
-            if(string.IsNullOrEmpty(genre))
+            if (string.IsNullOrEmpty(genre))
             {
                 return new NotFoundResult();
             }
 
             var movie = _getHighestRatedService.GetHighestRatedMoviePerGenre(genre);
-            if(movie == null)
+            if (movie == null)
             {
                 return new NotFoundResult();
             }
